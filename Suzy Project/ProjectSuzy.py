ef7220a90007239c734f6fdb311e240024984193
ins888 <73679447+ins888@users.noreply.github.com>
2020-12-09 17:11:28 +0700

    Update ProjectSuzy.py

--- Suzy Project/ProjectSuzy.py
+++ Suzy Project/ProjectSuzy.py
@@ -1,10 +1,26 @@
-import speech_recognition as sr  
+import speech_recognition as sr
+import requests
 from gtts import gTTS 
 from playsound import playsound 
 from datetime import datetime 
 
 
-r = sr.Recognizer() 
+r = sr.Recognizer()
+
+def weather():
+	url_w = 'http://api.openweathermap.org/data/2.5/weather?appid=0c42f7f6b53b244c78a418f4f181282a&q=Bangkok'
+	json_data_w = requests.get(url_w).json()
+	format_add_w = json_data_w['weather'][0]["main"]
+	if format_add_w == 'Clouds':
+		format_add_w = 'มีเมฆมาก'
+	return format_add_w
+
+def temp():
+	url_t = 'http://api.openweathermap.org/data/2.5/weather?appid=0c42f7f6b53b244c78a418f4f181282a&q=Bangkok'
+	json_data_t = requests.get(url_t).json()
+	format_add_t = json_data_t['main'][0]["temp"]
+	tempurature = (format_add_t-32)/1.8000
+	return tempurature
 
 with sr.Microphone() as source: 
 	playsound("./signal.mp3") 
@@ -22,8 +38,13 @@ with sr.Microphone() as source:
 		if text == "กี่โมงแล้ว":
 			now = datetime.now() 
 			text = now.strftime("ขณะนี้เวลา%Hนาฬิกา%Mนาที%Sวินาที")
+		if text == "อากาศเป็นไงบ้าง":
+			text = weather()
+		if text == "อุณหภูมิวันนี้":
+			text = temp()
+
 	except:
-		text = "ขอโทษค่ะฉันไม่เข้าใจในสิ่งที่คุณพูด"
+		text = "ขอโทษค่ะ ฉันไม่เข้าใจในสิ่งที่คุณพูด"
 	tts = gTTS(text, lang="th") 
 	tts.save("./answer.mp3") 
 	playsound("./answer.mp3")
