f7391a060b5050e707205d3e2a51c962a8c472ee
Hotelcombiend <71114488+Hotelcombiend@users.noreply.github.com>
2020-12-09 17:02:07 +0700

    Update suzy2.py

--- suzy2.py
+++ suzy2.py
@@ -1,31 +1,11 @@
-import speech_recognition as sr  #ใช้สำหรับแปลงเสียงพูดเป็นข้อความ
-from gtts import gTTS #ใช้สำหรับแปลงข้อความเป็นเสียงพูด
-from playsound import playsound #ใช้สำหรับเล่นไฟล์เสียง
-from datetime import datetime #ใช้สำหรับดูเวลาขณะนี้
+import speech_recognition as sr
+from gtts import gTTS
+from playsound import playsound
+from datetime import datetime
 
-
-r = sr.Recognizer() #เริ่มต้นInitiate
-with sr.Microphone() as source: 
-	audio = r.record(source, duration=5) #บันทึกเสียง 5 วินาที
-	text=""
-	try:
-		text = r.recognize_google(audio, language="th") #ส่งไปให้google cloud		
-		if "คุณชื่ออะไร" in text:
-			text = text.replace("คุณชื่ออะไร", "ฉันชื่อsuzy")
-		if "ผม" in text:
-			text = text.replace("ผม", "ฉันเองก็")
-		if "ครับ" in text:
-			text = text.replace("ครับ", "ค่ะ")
-		if "มุก" in text:
-			text = "ลาเร็วกว่าม้าเพราะลาไปก่อน555"
-		if "กี่โมงแล้ว" in text:
-			now = datetime.now() #รับค่าเวลาขณะนั้น
-			text = now.strftime("%H:%M:%S")
-		if "วันที่" in text:
-			now = datetime.now()
-			text = now.strftime("%x")
-	except:
-		text = "ขอโทษค่ะ"
-	tts = gTTS(text, lang="th") #ส่งไปให้google cloud
-	tts.save("./answer.mp3") #บันทึกเสียงที่ได้จากgoogle cloud
-	playsound("./answer.mp3")
+def suzy_speak(mess):
+	tts = gTTS (mess=text, lang="en")
+	name_f = "answer.mp3"
+	tts.save(name_f)
+	playsound.playsound(name_f)
+speak("hello world suzy")
