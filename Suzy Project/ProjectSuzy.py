b8a1e46dd411cc43bd3ca63f6da2e9daa9b2cb08
63070158 <71114538+63070158@users.noreply.github.com>
2020-12-09 16:19:18 +0700

    Add files via upload

---
+++ Suzy Project/ProjectSuzy.py
@@ -0,0 +1,29 @@
+import speech_recognition as sr  
+from gtts import gTTS 
+from playsound import playsound 
+from datetime import datetime 
+
+
+r = sr.Recognizer() 
+
+with sr.Microphone() as source: 
+	playsound("./signal.mp3") 
+	audio = r.record(source, duration=5) 
+	playsound("./signal.mp3") 
+
+	try:
+		text = r.recognize_google(audio, language="th") 
+		if "ชื่ออะไร" in text:
+			text = "สวัสดีฉันชื่อซูซี่"
+		if "ผม" in text:
+			text = text.replace("ผม", "ฉันเองก็")
+		if "ครับ" in text:
+			text = text.replace("ครับ", "ค่ะ")
+		if text == "กี่โมงแล้ว":
+			now = datetime.now() 
+			text = now.strftime("ขณะนี้เวลา%Hนาฬิกา%Mนาที%Sวินาที")
+	except:
+		text = "ขอโทษค่ะฉันไม่เข้าใจในสิ่งที่คุณพูด"
+	tts = gTTS(text, lang="th") 
+	tts.save("./answer.mp3") 
+	playsound("./answer.mp3")
